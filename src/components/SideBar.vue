5f7557df02ac7f2bd70abe5fc7ac61b26b30d63f
deadpe5 <65419535+deadpe5@users.noreply.github.com>
2023-04-13 00:21:47 +0300

    * Addded modal before deleting mesh.

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,12 +1,32 @@
 <template>
     <div class="diploma-sidebar">
         <v-card min-width="300px" elevation="2">
-            <v-card-title>Mesh list</v-card-title>
+            <div class="d-flex flex-row justify-space-between">
+                <v-card-title>Mesh list</v-card-title>
+                <v-btn :icon="'mdi-delete'" variant="text" @click="dialog = true" :disabled="visualisationStore.selectedMesh === null" />
+                <v-dialog v-model="dialog" width="auto">
+                    <v-card>
+                        <v-card-text>
+                            Are you sure?
+                        </v-card-text>
+                        <v-card-subtitle>
+                            Remove item '{{ visualisationStore.selectedMesh?.name }}'?
+                        </v-card-subtitle>
+                        <v-card-actions>
+                            <v-btn color="secondary" @click="dialog = false">Cancel</v-btn>
+                            <v-btn color="primary" @click="removeItem">Confirm</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+
+            </div>
             <v-container class="scrollable-list padding">
                 <v-list>
-                    <v-list-item v-for="item in visualisationStore.sceneItems" :key="item.id" :title="item.name" @click="selectItem(item as AbstractMesh)">
+                    <v-list-item v-for="item in visualisationStore.sceneItems" :key="item.id" :title="item.name"
+                        @click="selectItem(item as AbstractMesh)">
                         <template v-slot:append>
-                            <v-btn :icon="item.material?.alpha === 1.0 ? 'mdi-eye' : 'mdi-eye-off'" variant="text" @click.stop="toggleVisibility(item as AbstractMesh)"></v-btn>
+                            <v-btn :icon="item.material?.alpha === 1.0 ? 'mdi-eye' : 'mdi-eye-off'" variant="text"
+                                @click.stop="toggleVisibility(item as AbstractMesh)"></v-btn>
                         </template>
                     </v-list-item>
                 </v-list>
@@ -30,6 +50,8 @@ import { ref } from 'vue';
 import { useVisualisationStore } from '@/stores/visualisationStore';
 import type { AbstractMesh } from '@babylonjs/core';
 
+const dialog = ref(false)
+
 const fluidColor = ref('#00E5FF')
 const visualisationStore = useVisualisationStore()
 
@@ -39,6 +61,11 @@ function selectItem(item: AbstractMesh) {
 
 function toggleVisibility(item: AbstractMesh) {
     visualisationStore.toggleVisibility(item)
+}
+
+function removeItem() {
+    dialog.value = false
+    visualisationStore.removeSelectedSceneItem()
 }
 
 </script>
