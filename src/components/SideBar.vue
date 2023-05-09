b86b8ce840dd2981873ac3a69200a08f00279d39
deadpe5 <65419535+deadpe5@users.noreply.github.com>
2023-05-10 00:41:56 +0300

    * Added ability to change background environment. * Added ability to change box opacity. * Impelemented pause and restart buttons. * Renamed fields of RenderScene and added getters for them.

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -33,31 +33,57 @@
                 </v-list>
             </v-container>
             <v-divider></v-divider>
+            <v-card-title>Environment</v-card-title>
+            <v-container  class="padding">
+                <v-select label="Background" variant="solo" hide-details :items="ENVIRONMENT_NAMES" v-model="selectedEnv"></v-select>
+                <v-label class="mt-4">Box opacity</v-label>
+                <v-slider thumb-label :min="MIN_BOX_OPACITY" :max="MAX_BOX_OPACITY" :step="BOX_OPACITY_STEP"
+                @mouseover="onMouseEnterBoxOpacity" v-model="boxOpacity">
+                <template v-slot:append>
+                    <v-text-field v-model="boxOpacity" type="number" style="width: 100px" density="compact" hide-details
+                        :rules="boxRules" variant="outlined" :step="BOX_OPACITY_STEP"></v-text-field>
+                </template>
+            </v-slider>
+            <v-snackbar v-model="boxSnackbar" :timeout="timeout" color="red-accent-4">
+                {{ boxErrorMsg }}
+                <template v-slot:actions>
+                    <v-btn color="white" @click="boxSnackbar = false">
+                        <font-awesome-icon icon="fa-solid fa-circle-xmark" size="2xl" />
+                    </v-btn>
+                </template>
+            </v-snackbar>
+            </v-container>
+            <v-divider></v-divider>
             <v-card-title>Fluid properties</v-card-title>
             <v-container class="padding">
                 <v-checkbox label="Check box bounds" hide-details v-model="checkBounds"></v-checkbox>
-                <v-checkbox label="Auto rotate box" hide-details v-model="autoRotateBox" :disabled="!checkBounds"></v-checkbox>
+                <v-checkbox label="Auto rotate box" hide-details v-model="autoRotateBox"
+                    :disabled="!checkBounds || visualisationStore.isPaused"></v-checkbox>
                 <v-label>Fluid color & transparency</v-label>
                 <v-color-picker v-model="fluidColor" hide-canvas hide-inputs></v-color-picker>
                 <v-label class="mt-4">Particle count</v-label>
                 <v-slider thumb-label :min="MIN_PARTICLES_COUNT" :max="MAX_PARTICLES_COUNT" :step="PARTICLES_COUNT_STEP"
-                    @mouseover="onMouseEnter" v-model="particleCount">
+                    @mouseover="onMouseEnterParticle" v-model="particleCount">
                     <template v-slot:append>
                         <v-text-field v-model="particleCount" type="number" style="width: 100px" density="compact"
-                            hide-details :rules="rules" variant="outlined"></v-text-field>
+                            hide-details :rules="particleRules" variant="outlined"></v-text-field>
                     </template>
                 </v-slider>
-                <v-snackbar v-model="snackbar" :timeout="timeout" color="red-accent-4">
-                    {{ errorMsg }}
+                <v-snackbar v-model="particleSnackbar" :timeout="timeout" color="red-accent-4">
+                    {{ particleErrorMsg }}
                     <template v-slot:actions>
-                        <v-btn color="white" @click="snackbar = false">
+                        <v-btn color="white" @click="particleSnackbar = false">
                             <font-awesome-icon icon="fa-solid fa-circle-xmark" size="2xl" />
                         </v-btn>
                     </template>
                 </v-snackbar>
 
-                <v-btn width="100%" class="mb-4" color="secondary">Pause</v-btn>
-                <v-btn width="100%" color="primary">Restart</v-btn>
+                <v-btn 
+                    width="100%" class="mb-4" color="secondary" 
+                    :variant="visualisationStore.isPaused ? 'outlined' : 'elevated' " 
+                    :text="visualisationStore.isPaused ? 'Resume' : 'Pause' "
+                    @click="pause"/>
+                <v-btn width="100%" color="primary" @click="restart">Restart</v-btn>
             </v-container>
         </v-card>
     </div>
@@ -67,39 +93,80 @@
 import { ref, watch } from 'vue';
 import { useVisualisationStore } from '@/stores/visualisationStore';
 import type { AbstractMesh } from '@babylonjs/core';
-import { MIN_PARTICLES_COUNT, MAX_PARTICLES_COUNT, PARTICLES_COUNT_STEP } from '../constants'
+import {
+    MIN_PARTICLES_COUNT,
+    MAX_PARTICLES_COUNT,
+    PARTICLES_COUNT_STEP,
+    MIN_BOX_OPACITY,
+    MAX_BOX_OPACITY,
+    BOX_OPACITY_STEP,
+    DEFAULT_BOX_OPACITY,
+    ENVIRONMENT_NAMES
+} from '../constants'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+const visualisationStore = useVisualisationStore()
 
 const autoRotateBox = ref(false)
 const checkBounds = ref(true)
-const snackbar = ref(false)
 const timeout = ref(2000)
 const dialog = ref(false)
-const errorMsg = ref('')
-const particleCount = ref((MIN_PARTICLES_COUNT + MAX_PARTICLES_COUNT) / 2)
 
-const rules = [
-    (v: string | number) => Number(v) && Number.isInteger(Number(v)) || 'Particle count must be an integer',
+const selectedEnv = ref(ENVIRONMENT_NAMES[0])
+
+watch(selectedEnv, (value) => {
+    visualisationStore.changeEnvironment(value)
+})
+
+const boxErrorMsg = ref('')
+const boxSnackbar = ref(false)
+const boxOpacity = ref(DEFAULT_BOX_OPACITY)
+const boxRules = [
+    (v: string | number) => !isNaN(Number(v)) || 'Box opacity must be a number',
+    (v: string | number) => Number(v) >= MIN_BOX_OPACITY || `Box opacity must be greater than ${MIN_BOX_OPACITY}`,
+    (v: string | number) => Number(v) <= MAX_BOX_OPACITY || `Box opacity must be lower than ${MAX_BOX_OPACITY}`,
+]
+
+watch(boxOpacity, (value) => {
+    boxErrorMsg.value = ''
+    boxSnackbar.value = false
+    for (let i = 0; i < boxRules.length; i++) {
+        const rule = boxRules[i]
+        const result = rule(value)
+        if (typeof result === 'string') {
+            boxErrorMsg.value = result
+            boxSnackbar.value = true
+            break
+        }
+    }
+
+    visualisationStore.changeBoxOpacity(value)
+})
+
+const particleErrorMsg = ref('')
+const particleSnackbar = ref(false)
+const particleCount = ref((MIN_PARTICLES_COUNT + MAX_PARTICLES_COUNT) / 2)
+const particleRules = [
+    (v: string | number) => !isNaN(Number(v)) && Number.isInteger(Number(v)) || 'Particle count must be an integer',
     (v: string | number) => Number(v) >= MIN_PARTICLES_COUNT || `Particle count must be greater than ${MIN_PARTICLES_COUNT}`,
     (v: string | number) => Number(v) <= MAX_PARTICLES_COUNT || `Particle count must be lower than ${MAX_PARTICLES_COUNT}`,
 ]
 
-watch(particleCount, () => {
-    errorMsg.value = ''
-    snackbar.value = false
-    for (let i = 0; i < rules.length; i++) {
-        const rule = rules[i]
-        const result = rule(particleCount.value)
+watch(particleCount, (value) => {
+    particleErrorMsg.value = ''
+    particleSnackbar.value = false
+    for (let i = 0; i < particleRules.length; i++) {
+        const rule = particleRules[i]
+        const result = rule(value)
         if (typeof result === 'string') {
-            errorMsg.value = result
-            snackbar.value = true
+            particleErrorMsg.value = result
+            particleSnackbar.value = true
             break
         }
     }
 })
 
 watch(autoRotateBox, isActive => {
-    visualisationStore.autoRotateBox(isActive)
+    visualisationStore.enableAutoRotateBox(isActive)
 })
 
 watch(checkBounds, isActive => {
@@ -111,7 +178,6 @@ watch(checkBounds, isActive => {
 })
 
 const fluidColor = ref('#00E5FF')
-const visualisationStore = useVisualisationStore()
 
 function selectItem(item: AbstractMesh) {
     visualisationStore.select(item)
@@ -126,12 +192,29 @@ function removeItem() {
     visualisationStore.removeSelectedSceneItem()
 }
 
-function onMouseEnter() {
-    if (errorMsg.value !== '') {
-        snackbar.value = true
+function pause() {
+    if (visualisationStore.isPaused && autoRotateBox.value) {
+        visualisationStore.enableAutoRotateBox(true)
+    }
+    visualisationStore.pauseSimulation()
+}
+
+function restart() {
+    autoRotateBox.value = false
+    visualisationStore.restartSimulation()
+}
+
+function onMouseEnterParticle() {
+    if (particleErrorMsg.value !== '') {
+        particleSnackbar.value = true
     }
 }
 
+function onMouseEnterBoxOpacity() {
+    if (boxErrorMsg.value !== '') {
+        boxSnackbar.value = true
+    }
+}
 </script>
 <style>
 .scrollable-list {
--- src/visualisation/RenderScene.ts
+++ src/visualisation/RenderScene.ts
@@ -15,53 +15,54 @@ import RenderCamera from './CameraWrapper'
 import { useVisualisationStore } from '@/stores/visualisationStore'
 import MeshManager from './MeshManager'
 import { FluidVisualisation } from './FluidVisualisation'
+import { ENVIRONMENT_FILENAMES, ENVIRONMENT_NAMES } from '@/constants'
 
 class RenderScene {
   private readonly canvas: HTMLCanvasElement
-  private readonly engine: Engine
-  private readonly scene: Scene
-  private readonly camera: RenderCamera
-  private readonly gizmoManager: GizmoManager
-  private readonly meshManager: MeshManager
+  private readonly _engine: Engine
+  private readonly _scene: Scene
+  private readonly _camera: RenderCamera
+  private readonly _gizmoManager: GizmoManager
+  private readonly _meshManager: MeshManager
   private readonly visualisationStore = useVisualisationStore()
   private readonly sceneLight: HemisphericLight
   private readonly cameraLight: PointLight
-  private readonly fluidVisualisation: FluidVisualisation
+  private readonly _fluidVisualisation: FluidVisualisation
 
   constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas
-    this.engine = new Engine(this.canvas)
-    this.scene = new Scene(this.engine)
-    this.scene.useRightHandedSystem = true
+    this._engine = new Engine(this.canvas)
+    this._scene = new Scene(this._engine)
+    this._scene.useRightHandedSystem = true
     this.setEnvironment()
-    this.gizmoManager = new GizmoManager(this.scene, 2)
-    this.gizmoManager.positionGizmoEnabled = true
-    this.gizmoManager.rotationGizmoEnabled = true
-    this.gizmoManager.enableAutoPicking = false
+    this._gizmoManager = new GizmoManager(this._scene, 2)
+    this._gizmoManager.positionGizmoEnabled = true
+    this._gizmoManager.rotationGizmoEnabled = true
+    this._gizmoManager.enableAutoPicking = false
 
     this.canvas.addEventListener('wheel', (evt) => evt.preventDefault())
-    this.scene.clearColor = new Color4(1, 1, 1, 1)
+    this._scene.clearColor = new Color4(1, 1, 1, 1)
 
-    this.camera = new RenderCamera(this.scene, this.canvas)
+    this._camera = new RenderCamera(this._scene, this.canvas)
 
-    this.sceneLight = new HemisphericLight('sceneLight', new Vector3(0, 0, 1), this.scene)
+    this.sceneLight = new HemisphericLight('sceneLight', new Vector3(0, 0, 1), this._scene)
     this.sceneLight.intensity = 0.3
 
-    this.cameraLight = new PointLight('cameraLight', this.camera.position, this.scene)
+    this.cameraLight = new PointLight('cameraLight', this._camera.position, this._scene)
     this.cameraLight.intensity = 1
 
-    this.meshManager = new MeshManager(this.scene)
+    this._meshManager = new MeshManager(this._scene)
 
-    this.fluidVisualisation = new FluidVisualisation(this)
-    this.fluidVisualisation.run()
-    this.scene.onDisposeObservable.add(() => {
-      this.fluidVisualisation.dispose()
+    this._fluidVisualisation = new FluidVisualisation(this)
+    this._fluidVisualisation.run()
+    this._scene.onDisposeObservable.add(() => {
+      this._fluidVisualisation.dispose()
     })
 
-    this.engine.runRenderLoop(() => {
-      const fps = this.engine.getFps()
+    this._engine.runRenderLoop(() => {
+      const fps = this._engine.getFps()
       this.visualisationStore.setFPS(fps)
-      this.scene.render()
+      this._scene.render()
     })
 
     this.scene.onKeyboardObservable.add((kbInfo) => {
@@ -72,7 +73,7 @@ class RenderScene {
 
     if (window) {
       const resize = () => {
-        this.scene.getEngine().resize()
+        this._scene.getEngine().resize()
       }
 
       window.addEventListener('resize', resize)
@@ -81,69 +82,71 @@ class RenderScene {
     this.addOnPointerObservable()
   }
 
-  public get getFluidVisualisation() {
-    return this.fluidVisualisation
+  public get fluidVisualisation() {
+    return this._fluidVisualisation
+  }
+
+  public get scene(): Scene {
+    return this._scene
+  }
+
+  public get engine(): Engine {
+    return this._engine
+  }
+
+  public get activeCamera(): RenderCamera {
+    return this._camera
+  }
+
+  public get meshManager(): MeshManager {
+    return this._meshManager
+  }
+
+  public get gizmoManager(): GizmoManager {
+    return this._gizmoManager
   }
 
   changeView(view: string) {
     const PI = Math.PI
     switch (view) {
       case '+X':
-        this.camera.rotateCamera(0, PI / 2)
+        this._camera.rotateCamera(0, PI / 2)
         return
       case '-X':
-        this.camera.rotateCamera(PI, PI / 2)
+        this._camera.rotateCamera(PI, PI / 2)
         return
       case '+Y':
-        this.camera.rotateCamera(-PI / 2, PI / 2)
+        this._camera.rotateCamera(-PI / 2, PI / 2)
         return
       case '-Y':
-        this.camera.rotateCamera(PI / 2, PI / 2)
+        this._camera.rotateCamera(PI / 2, PI / 2)
         return
       case '+Z':
-        this.camera.rotateCamera(PI / 2, 0)
+        this._camera.rotateCamera(PI / 2, 0)
         return
       case '-Z':
-        this.camera.rotateCamera(PI / 2, PI)
+        this._camera.rotateCamera(PI / 2, PI)
         return
       case 'XYZ':
-        this.camera.rotateCamera(-PI / 4, PI / 3)
+        this._camera.rotateCamera(-PI / 4, PI / 3)
         return
     }
   }
 
-  setEnvironment() {
-    this.scene.environmentTexture =
-      CubeTexture.CreateFromPrefilteredData("https://playground.babylonjs.com/textures/country.env", this.scene);
+  setEnvironment(env?: string) {
+    const envFileName = env ? ENVIRONMENT_FILENAMES[ENVIRONMENT_NAMES.indexOf(env)] : ENVIRONMENT_FILENAMES[0]
 
-    const skybox = this.scene.createDefaultSkybox(this.scene.environmentTexture);
+    this._scene.environmentTexture =
+      CubeTexture.CreateFromPrefilteredData(`/environments/${envFileName}`, this._scene);
+
+    const skybox = this._scene.createDefaultSkybox(this._scene.environmentTexture);
     skybox!.rotation.x = Math.PI / 2
-  }
-  
-  getScene(): Scene {
-    return this.scene
-  }
-
-  getEngine(): Engine {
-    return this.engine
-  }
-
-  getActiveCamera(): RenderCamera {
-    return this.camera
-  }
-
-  getMeshManager(): MeshManager {
-    return this.meshManager
-  }
-
-  getGizmoManager(): GizmoManager {
-    return this.gizmoManager
   }
 
   private addOnPointerObservable() {
     let pointerX = 0
     let pointerY = 0
-    this.scene.onPointerObservable.add((evt) => {
+    this._scene.onPointerObservable.add((evt) => {
       if (evt.type === PointerEventTypes.POINTERDOWN) {
         pointerX = Math.round(evt.event.clientX)
         pointerY = Math.round(evt.event.clientY)
@@ -153,8 +156,8 @@ class RenderScene {
       if (evt.type === PointerEventTypes.POINTERUP) {
         if (Math.round(evt.event.clientX) === pointerX &&
           Math.round(evt.event.clientY) === pointerY) {
-          const pickingRay = this.scene.createPickingRay(pointerX, pointerY, Matrix.Identity(), this.camera)
-          const hitInfo = this.scene.pickWithRay(pickingRay)
+          const pickingRay = this._scene.createPickingRay(pointerX, pointerY, Matrix.Identity(), this._camera)
+          const hitInfo = this._scene.pickWithRay(pickingRay)
           if (hitInfo && hitInfo.hit) {
             this.visualisationStore.select(hitInfo.pickedMesh)
           }
